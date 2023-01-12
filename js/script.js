3df2da2c3efd1599c08664eff88b55b93436978a
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-01-12 19:17:51 +0000

    Add if statement for computerMove

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+// // **
+// // let randomNumber = Math.floor(Math.random() * (19 - 11 + 1) + 11)
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log("Wylosowana licza to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else {
+  computerMove = "nożyce";
+}
+
+printMessage("Mój ruch to " + computerMove);
